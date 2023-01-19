080fdc772bc83408e3766f8565ad2cf6f9196b8f
thakkar-nit <thakkar.nit@northeastern.edu>
2023-01-19 16:53:55 -0500

    final commit

---
+++ LAB0/build/tutorials/catkin_generated/installspace/rsn_talker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+
+def talker():
+    publisher=rospy.Publisher('chatter',String,queue_size=10)
+    rospy.init_node('RSN_talker',anonymous=True)
+    rate=rospy.Rate(10)
+    while not rospy.is_shutdown():
+        string_example="I have completed the first assignment"
+        rospy.loginfo(string_example)
+        publisher.publish(string_example)
+        rate.sleep()
+
+
+
+if __name__=='__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
+    
