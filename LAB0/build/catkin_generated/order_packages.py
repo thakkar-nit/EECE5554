5e4a0a4cbc6e470b53e96e89b0734177ad59b41a
thakkar-nit <122404993+thakkar-nit@users.noreply.github.com>
2023-01-19 16:55:56 -0500

    Delete LAB0/build directory

--- LAB0/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/nitin/EECE5554/LAB0/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/nitin/nitin_ros/devel;/home/nitin/udemy_ros/devel;/home/nitin/turtlebot3/devel;/opt/ros/noetic'.split(';') if '/home/nitin/nitin_ros/devel;/home/nitin/udemy_ros/devel;/home/nitin/turtlebot3/devel;/opt/ros/noetic' != '' else []
