395e63fa359564c7eda50ae26b020b400d326be8
thakkar-nit <thakkar.nit@northeastern.edu>
2023-01-22 00:38:11 -0500

    final commit

--- LAB0/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nitin/EECE5554/LAB0/src
-# Build directory: /home/nitin/EECE5554/LAB0/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("tutorials")
--- LAB0/build/tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nitin/EECE5554/LAB0/src/tutorials
-# Build directory: /home/nitin/EECE5554/LAB0/build/tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
